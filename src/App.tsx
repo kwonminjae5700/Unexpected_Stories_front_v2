bf96a97d1db99a33665b676235acfdccdc886f78
kwon5700 <kwon777071@gmail.com>
2025-01-04 18:23:55 +0900

    feat : 렌더링 페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import RenderingPage from "./pages/RenderingPage"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import styled from "styled-components"
-import BusStopBackgroundImage from "./assets/BusStopBackgroundImage.png"
+import RenderingPage from './pages/RenderingPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
+import BusStopBackgroundImage from './assets/BusStopBackgroundImage.png'
+import './font.css'
 
 const App = () => {
-
   return (
     <BrowserRouter>
       <BackgroundBox>
         <Routes>
-          <Route path="/" element={<RenderingPage />}/>
+          <Route path="/" element={<RenderingPage />} />
         </Routes>
       </BackgroundBox>
     </BrowserRouter>
   )
 }
 
-export const BackgroundBox = styled.div`
+const BackgroundBox = styled.div`
   width: 100%;
-  max-width : 480px;
+  max-width: 480px;
   height: 100%;
   background-image: url(${BusStopBackgroundImage});
   background-size: 100% 100%;
-  margin : 0 auto;
+  margin: 0 auto;
 `
 
 export default App
--- src/pages/RenderingPage.tsx
+++ src/pages/RenderingPage.tsx
@@ -1,8 +1,23 @@
-
+import * as S from '../styles/render'
 
 const RenderingPage = () => {
-  return <></>
+  return (
+    <S.RenderLayout>
+      <S.TitleBox>
+        <S.TitleText>어쩌다</S.TitleText>
+        <S.TitleText>발견한</S.TitleText>
+        <S.TitleText>이야기</S.TitleText>
+      </S.TitleBox>
+
+      <S.ParagraphBox>
+        <S.Paragraph>우리 동네를 스쳐 지나가는 사람들,</S.Paragraph>
+        <S.Paragraph>그들이 주인공이 되어 들려주는 이야기.</S.Paragraph>
+      </S.ParagraphBox>
+
+      <S.StartBtn>시작하기</S.StartBtn>
+    </S.RenderLayout>
+  )
+
 }
 
 export default RenderingPage
-  
